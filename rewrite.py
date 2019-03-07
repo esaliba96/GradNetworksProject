f15145c9315c4a16657c3e12419cb987e4c76555
Nick Papadakis <nicholas.t.papadakis@gmail.com>
2019-03-07 11:02:15 -0800

    Added python script to test wtih squid

---
+++ rewrite.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import sys
+import logging
+
+logging.basicConfig(filename='squid-redirect.log',level=logging.DEBUG)
+
+request  = sys.stdin.readline()
+while request:
+    [ch_id,url,ipaddr,method,user]=request.split()
+    logging.debug(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ': ' + request +'\n')
+    response  = ch_id + ' OK'
+    if '.jpg' in url:
+        response +=  ' rewrite-url=http://127.0.0.0/image.jpg'
+    response += '\n'
+    sys.stdout.write(response)
+    sys.stdout.flush()
+    request = sys.stdin.readline()
+
